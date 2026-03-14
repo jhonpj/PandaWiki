b70bdb26bc91f67635d55f61512a9b5db65c240e
jhonpj <pengjingmine@163.com>
2026-03-14 18:20:49 +0000

    Sync from chaitin/PandaWiki (clean LFS) 2026-03-14

---
+++ backend/handler/mq/provider.go
@@ -0,0 +1,37 @@
+package mq
+
+import (
+	"github.com/google/wire"
+
+	"github.com/chaitin/panda-wiki/repo/ipdb"
+	"github.com/chaitin/panda-wiki/repo/mq"
+	"github.com/chaitin/panda-wiki/repo/pg"
+	"github.com/chaitin/panda-wiki/store/rag"
+	"github.com/chaitin/panda-wiki/store/s3"
+	"github.com/chaitin/panda-wiki/usecase"
+)
+
+type MQHandlers struct {
+	RAGMQHandler        *RAGMQHandler
+	RagDocUpdateHandler *RagDocUpdateHandler
+	StatCronHandler     *CronHandler
+}
+
+var ProviderSet = wire.NewSet(
+	pg.ProviderSet,
+	rag.ProviderSet,
+	mq.ProviderSet,
+	ipdb.ProviderSet,
+	s3.ProviderSet,
+
+	usecase.NewLLMUsecase,
+	usecase.NewStatUseCase,
+	usecase.NewNodeUsecase,
+	usecase.NewModelUsecase,
+
+	NewRAGMQHandler,
+	NewRagDocUpdateHandler,
+	NewCronHandler,
+
+	wire.Struct(new(MQHandlers), "*"),
+)
