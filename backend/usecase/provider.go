7f49186ef3c5ced7dba36ebc7bd1e4fa216248fe
jhonpj <pengjingmine@163.com>
2026-05-18 19:21:06 +0000

    Sync from chaitin/PandaWiki (clean LFS) 2026-05-18

---
+++ backend/usecase/provider.go
@@ -0,0 +1,39 @@
+package usecase
+
+import (
+	"github.com/google/wire"
+
+	"github.com/chaitin/panda-wiki/repo/ipdb"
+	mqRepo "github.com/chaitin/panda-wiki/repo/mq"
+	"github.com/chaitin/panda-wiki/repo/pg"
+	"github.com/chaitin/panda-wiki/store/rag"
+	"github.com/chaitin/panda-wiki/store/s3"
+)
+
+var ProviderSet = wire.NewSet(
+	pg.ProviderSet,
+	mqRepo.ProviderSet,
+	ipdb.ProviderSet,
+	rag.ProviderSet,
+	s3.ProviderSet,
+
+	NewLLMUsecase,
+	NewNodeUsecase,
+	NewAppUsecase,
+	NewConversationUsecase,
+	NewUserUsecase,
+	NewModelUsecase,
+	NewKnowledgeBaseUsecase,
+	NewChatUsecase,
+	NewCrawlerUsecase,
+	NewCreationUsecase,
+	NewFileUsecase,
+	NewSitemapUsecase,
+	NewStatUseCase,
+	NewCommentUsecase,
+	NewWechatUsecase,
+	NewWecomUsecase,
+	NewWechatAppUsecase,
+	NewAuthUsecase,
+	NewNavUsecase,
+)
